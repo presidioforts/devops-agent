07794ca7a06e2ec754a899a839a916d09f1d6715
presidioforts <presidio2025@gmail.com>
2025-05-11 12:13:30 -0700

    Create organizeModel.sh

---
+++ organizeModel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Set working directory to the location of the zip file
+cd /apps/logAnalyzer
+
+# Create the target directory if it doesn't exist
+mkdir -p breakfix-kb-model/all-mpnet-base-v2
+
+# Unzip the model into the target directory
+unzip -o all-mpnet-base.zip -d breakfix-kb-model/all-mpnet-base-v2
+
+# If the zip contains a subfolder, move files up one level (optional cleanup)
+if [ -d breakfix-kb-model/all-mpnet-base-v2/all-mpnet-base-v2 ]; then
+    mv breakfix-kb-model/all-mpnet-base-v2/all-mpnet-base-v2/* breakfix-kb-model/all-mpnet-base-v2/
+    rmdir breakfix-kb-model/all-mpnet-base-v2/all-mpnet-base-v2
+fi
+
+echo "Model extracted to breakfix-kb-model/all-mpnet-base-v2/"
