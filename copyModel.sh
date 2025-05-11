52267244210287f7f1662f9259b5115030de8b75
presidioforts <presidio2025@gmail.com>
2025-05-11 13:35:14 -0700

    Create copyModel.sh

---
+++ copyModel.sh
@@ -0,0 +1,2 @@
+mkdir -p /apps/logAnalyzer/breakfix-kb-model/all-mpnet-base-v2
+cp -r /apps/logAnalyzer/all-mpnet-base/84f2bcc00d77236f9e89c8a360a00fb1139bf47d5/* /apps/logAnalyzer/breakfix-kb-model/all-mpnet-base-v2/
